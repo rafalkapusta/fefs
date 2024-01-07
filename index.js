b4d7ba0a9fb420a487af84e7cd5c1a90f04b91cb
Rafał Kapusta <rfkapusta@gmail.com>
2024-01-07 13:21:04 +0000

    sqlite database

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 const port = 3000;
 
+const db = new sqlite3.Database('./users.db', (err) => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log('Connected to the SQlite database.');
+    }
+});
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+    // res.sendFile(path.join(__dirname, 'index.html'));
+    db.serialize(() => {
+        db.run('CREATE TABLE IF NOT EXISTS users(name TEXT, email TEXT )')
+            .run(`INSERT INTO users (name, email) VALUES ('a', 'a@a.com'),('b','b@b.com')`)
+            .all(`SELECT * FROM users`, (err, rows) => {
+                if(err) {
+                    console.error(err)
+                }
+                const data = JSON.stringify(rows);
+                res.send(data);
+            })
+    })
+
 });
 
 app.listen(port, () => {
-  console.log(`Servers started on  ${port}`);
+    console.log(`Server started on  ${port}`);
 })
