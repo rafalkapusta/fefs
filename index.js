cc6b04c7d87ab46f3b9f4996b05ae0ea31ee32b6
Rafał Kapusta <rfkapusta@gmail.com>
2024-01-07 13:31:37 +0000

    close db

--- index.js
+++ index.js
@@ -14,20 +14,22 @@ const db = new sqlite3.Database('./users.db', (err) => {
 });
 
 app.get('/', (req, res) => {
-    // res.sendFile(path.join(__dirname, 'index.html'));
-    db.serialize(() => {
-        db.run('CREATE TABLE IF NOT EXISTS users(name TEXT, email TEXT )')
-            .run(`INSERT INTO users (name, email) VALUES ('a', 'a@a.com'),('b','b@b.com')`)
-            .all(`SELECT * FROM users`, (err, rows) => {
-                if(err) {
-                    console.error(err)
-                }
-                const data = JSON.stringify(rows);
-                res.send(data);
-            })
-    })
+    res.sendFile(path.join(__dirname, 'index.html'));
+    //db.serialize(() => {
+      //  db.run('CREATE TABLE IF NOT EXISTS users(name TEXT, email TEXT )')
+        //    .run(`INSERT INTO users (name, email) VALUES ('a', 'a@a.com'),('b','b@b.com')`)
+          //  .all(`SELECT * FROM users`, (err, rows) => {
+            //    if(err) {
+              //      console.error(err)
+                //}
+                //const data = JSON.stringify(rows);
+                //res.send(data);
+            //})
+    //})
 
 });
+
+db.close();
 
 app.listen(port, () => {
     console.log(`Server started on  ${port}`);
